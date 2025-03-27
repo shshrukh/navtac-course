c43dd3a861d46d82007f4280618a0e1175f82847
shshrukh <shshrukh.arbaz@gmail.com>
2025-03-27 15:41:29 +0500

    switch statement

--- DaySix/logic.js
+++ DaySix/logic.js
@@ -2,3 +2,26 @@
 const age = 18 ;
 let res = age > 19? 'Ok' : 'not Ok';
 console.log(res);
+
+
+//templet leterals
+
+let fname = "alex";
+let result = `my age is ${fname} and my age is ${age}`;
+console.log(result);
+
+//switch statement
+let day = 1;
+switch (day) {
+    case 1:
+        console.log('monday');
+        
+        break;
+    case 2:
+        console.log('sunday');
+        
+    default:
+        console.log('notDay');
+        
+        break;
+}
