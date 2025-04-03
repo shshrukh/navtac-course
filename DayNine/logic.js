142756e8e93a02564c8e8ab4e963795421fa6897
shshrukh <shshrukh.arbaz@gmail.com>
2025-04-03 22:57:47 +0500

    changes

--- DayNine/logic.js
+++ DayNine/logic.js
@@ -16,3 +16,35 @@ console.log(userOne);
     },
 }
 console.log(userTwo.fullName())
+
+// let we have multipal of users like thousand users then? we have thousand of methods.
+//still this is not good approch,
+
+usersMethod = function(){
+    return this.fName + ' '+ this.lName;
+}
+userThere = {
+    fName : 'Rashid',
+    lName : 'saleem',
+    age : 34,
+    fullName : usersMethod,
+}
+console.log(userThere, userThere.fullName());
+
+// now suppose we have multipal of methods then ?
+
+usersMethods = {
+    methodOne : function(){
+        return this.fName +' '+ this.lName;
+    },
+    methodTwo : function(){
+        return 'hello ' + this.fName + ' ' + this.lName;
+    }
+}
+userFour = {
+    fName : 'sohail',
+    lName : 'hameed',
+    age : 89,
+    genMethods : usersMethods.methodTwo
+}
+console.log(userFour, userFour.genMethods());
