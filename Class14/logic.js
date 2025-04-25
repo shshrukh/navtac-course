d81a2caec551195280f31f293f8d4d32497fa449
shshrukh <shshrukh.arbaz@gmail.com>
2025-04-25 10:33:21 +0500

    some more details about premitive data type

---
+++ Class14/logic.js
@@ -0,0 +1,11 @@
+// some more deatails about non premitive data type. arrays.
+// in  arrays we have store multipal data types 
+
+let randomDataTypes = ['abc', 2, null, undefined, true, false, [{fName : 'shah'}], {age : 32, cars : ['BMW', 'GLI']}]
+
+// advantages we csn assess the value of each iteam in arry useing index. This is methods in array to assess each value in given any arry
+
+const valueOne = randomDataTypes[0] ;
+const valueTwo = randomDataTypes[1] ;
+const valueThree = randomDataTypes[6] ;
+console.log(valueOne, valueTwo, valueThree);
